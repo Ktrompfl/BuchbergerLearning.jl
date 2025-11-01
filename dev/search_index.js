a45f2e28173c71f2eaf79f708067f597b1195b76
Documenter.jl <documenter@juliadocs.github.io>
2025-11-01 21:09:03 +0000

    build based on f636940

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#BuchbergerLearning","page":"Home","title":"BuchbergerLearning","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BuchbergerLearning.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
